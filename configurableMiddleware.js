a4d2cdc226d8b39c8fcd9e5856bd698ef7e7da23
anjali991 <anjalims6190@gmail.com>
2024-05-09 14:06:52 +0530

    assigmnent1

---
+++ configurableMiddleware.js
@@ -0,0 +1,8 @@
+function configurableMiddleware(config) {
+    return function(req, res, next) {
+      console.log(`Configurable middleware executed with config: ${config}`);
+      next();
+    };
+  }
+  
+  module.exports = configurableMiddleware;
